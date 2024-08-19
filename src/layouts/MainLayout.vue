6d07922a40aa8fe50bae85a5f71c61ea17752bd2
Felipe Barreto <felipebarreto148@gmail.com>
2024-08-19 16:10:26 -0300

    feat: set responsive sidebar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,10 @@
 
     <q-header elevated class="bg-primary text-white">
       <q-toolbar class="flex justify-between items-center q-py-sm">
+        <section class="flex items-center">
+          <q-btn flat @click="sidebar = !sidebar" round dense icon="menu" class="md-hide" />
           <img class="app-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png">
+        </section>
 
         <section class="flex items-center">
           <span class="text-h6 text-weight-bold q-px-md">Felipe Barreto</span>
@@ -12,7 +15,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above side="left" bordered class="bg-indigo-10 text-white q-pa-md">
+    <q-drawer v-model="sidebar" show-if-above side="left" bordered class="bg-indigo-10 text-white q-pa-md">
       <q-list>
         <template v-for="(item, index) in menu" :key="index">
           <q-item clickable :to="item.route" exact active-class="bg-blue-10 text-white">
@@ -37,6 +40,9 @@
 </template>
 
 <script setup lang="ts">
+// Core
+import { ref } from 'vue';
+
 defineOptions({
   name: 'MainLayout'
 });
@@ -63,6 +69,8 @@ const menu = [
     route: '/sobre'
   }
 ]
+
+const sidebar = ref(false);
 </script>
 
 <style scoped lang="scss">
